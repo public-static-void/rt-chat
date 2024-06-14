edf7d13cec5dfddeee87f8853e9bf093f17a4a59
Vadim Titov <vadim.titov.hh@gmail.com>
2024-06-14 18:08:25 +0200

    feat: added ui and auth context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { authUser, isLoading } = useAuthContext();
+  if (isLoading) {
+    return null;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUp /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,66 @@
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type AuthUserType = {
+  id: string;
+  fullName: string;
+  email: string;
+  profilePic: string;
+  gender: string;
+};
+
+const AuthContext = createContext<{
+  authUser: AuthUserType | null;
+  setAuthUser: Dispatch<SetStateAction<AuthUserType | null>>;
+  isLoading: boolean;
+}>({
+  authUser: null,
+  setAuthUser: () => {},
+  isLoading: true,
+});
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [authUser, setAuthUser] = useState<AuthUserType | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // logic will go here
+  useEffect(() => {
+    const fetchAuthUser = async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+        setAuthUser(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchAuthUser();
+  }, []);
+  return (
+    <AuthContext.Provider
+      value={{
+        authUser,
+        isLoading,
+        setAuthUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
