d8369a73693a8d65e7b4946dcc0a42503b29dc03
Vadim Titov <vadim.titov.hh@gmail.com>
2024-06-14 18:57:17 +0200

    feat: added login and logout functionality to frontend

--- frontend/src/components/GenderCheckbox.tsx
+++ frontend/src/components/GenderCheckbox.tsx
@@ -1,19 +1,35 @@
-const GenderCheckbox = () => {
-	return (
-		<div className='flex'>
-			<div className='form-control'>
-				<label className={`label gap-2 cursor-pointer`}>
-					<span className='label-text'>Male</span>
-					<input type='checkbox' className='checkbox border-slate-900' />
-				</label>
-			</div>
-			<div className='form-control'>
-				<label className={`label gap-2 cursor-pointer`}>
-					<span className='label-text'>Female</span>
-					<input type='checkbox' className='checkbox border-slate-900' />
-				</label>
-			</div>
-		</div>
-	);
+const GenderCheckbox = ({
+  selectedGender,
+  onCheckboxChange,
+}: {
+  selectedGender: string;
+  onCheckboxChange: (gender: "male" | "female") => void;
+}) => {
+  return (
+    <div className="flex">
+      <div className="form-control">
+        <label className={`label gap-2 cursor-pointer`}>
+          <span className="label-text">Male</span>
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
+        </label>
+      </div>
+      <div className="form-control">
+        <label className={`label gap-2 cursor-pointer`}>
+          <span className="label-text">Female</span>
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
+        </label>
+      </div>
+    </div>
+  );
 };
 export default GenderCheckbox;
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -7,6 +7,7 @@ import {
   useEffect,
   useState,
 } from "react";
+import toast from "react-hot-toast";
 
 type AuthUserType = {
   id: string;
@@ -44,8 +45,9 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
           throw new Error(data.error);
         }
         setAuthUser(data);
-      } catch (error) {
-        console.error(error);
+      } catch (error: any) {
+        console.error(error.message);
+        toast.error(error.message);
       } finally {
         setIsLoading(false);
       }
